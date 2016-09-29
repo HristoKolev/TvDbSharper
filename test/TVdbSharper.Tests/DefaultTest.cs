bf2b8a1b5720373218c8bbcc80f4d8ce1e5bc571
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-09-29 13:41:13 +0300

    added testing libraries and making them work

---
+++ test/TVdbSharper.Tests/DefaultTest.cs
@@ -0,0 +1,19 @@
+﻿namespace TVdbSharper.Tests
+{
+    using Xunit;
+
+    public class DefaultTest
+    {
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
