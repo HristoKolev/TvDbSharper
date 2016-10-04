2050143b723161ebc3320ccd0beff8cdbc7afb84
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-10-04 14:03:10 +0300

    added missing model

---
+++ src/TvDbSharper/RestClient/Models/EpisodesSummary.cs
@@ -0,0 +1,13 @@
+namespace TvDbSharper.RestClient.Models
+{
+    public class EpisodesSummary
+    {
+        public string AiredEpisodes { get; set; }
+
+        public string[] AiredSeasons { get; set; }
+
+        public string DvdEpisodes { get; set; }
+
+        public string[] DvdSeasons { get; set; }
+    }
+}
