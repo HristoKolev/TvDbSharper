25e4aea425c329da0baaf40295733af664c2fbd5
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-10-06 18:28:16 +0300

    added SearchClient overload methods

--- src/TvDbSharper/JsonApi/Search/SearchClient.cs
+++ src/TvDbSharper/JsonApi/Search/SearchClient.cs
@@ -23,5 +23,22 @@
 
             return await this.GetAsync<SeriesSearchResult[]>(requestUri, cancellationToken);
         }
+
+        public async Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesByImdbIdAsync(string imdbId, CancellationToken cancellationToken)
+        {
+            return await this.SearchSeriesAsync(imdbId, SearchParameter.ImdbId, cancellationToken);
+        }
+
+        public async Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesByNameAsync(string name, CancellationToken cancellationToken)
+        {
+            return await this.SearchSeriesAsync(name, SearchParameter.Name, cancellationToken);
+        }
+
+        public async Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesByZap2ItIdAsync(
+            string zap2ItId,
+            CancellationToken cancellationToken)
+        {
+            return await this.SearchSeriesAsync(zap2ItId, SearchParameter.Zap2itId, cancellationToken);
+        }
     }
 }
--- test/TvDbSharper.Tests/SearchClientTest.cs
+++ test/TvDbSharper.Tests/SearchClientTest.cs
@@ -36,5 +36,74 @@
 
             Assert.Equal(expectedData, responseData);
         }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async void SearchSeriesByImdbIdAsync_Makes_The_Right_Request()
+        {
+            var jsonClient = Substitute.For<IJsonClient>();
+            var client = new SearchClient(jsonClient);
+
+            const string ImdbId = "tt0436992";
+
+            const string Route = "/search/series?imdbId=tt0436992";
+
+            var expectedData = new TvDbResponse<SeriesSearchResult[]>();
+
+            jsonClient.GetJsonAsync<TvDbResponse<SeriesSearchResult[]>>(Route, CancellationToken.None).Returns(expectedData);
+
+            var responseData = await client.SearchSeriesByImdbIdAsync(ImdbId, CancellationToken.None);
+
+            await jsonClient.Received().GetJsonAsync<TvDbResponse<SeriesSearchResult[]>>(Route, CancellationToken.None);
+
+            Assert.Equal(expectedData, responseData);
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async void SearchSeriesByNameAsync_Makes_The_Right_Request()
+        {
+            var jsonClient = Substitute.For<IJsonClient>();
+            var client = new SearchClient(jsonClient);
+
+            const string Name = "Doctor Who";
+
+            const string Route = "/search/series?name=Doctor Who";
+
+            var expectedData = new TvDbResponse<SeriesSearchResult[]>();
+
+            jsonClient.GetJsonAsync<TvDbResponse<SeriesSearchResult[]>>(Route, CancellationToken.None).Returns(expectedData);
+
+            var responseData = await client.SearchSeriesByNameAsync(Name, CancellationToken.None);
+
+            await jsonClient.Received().GetJsonAsync<TvDbResponse<SeriesSearchResult[]>>(Route, CancellationToken.None);
+
+            Assert.Equal(expectedData, responseData);
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async void SearchSeriesByZap2itIdAsync_Makes_The_Right_Request()
+        {
+            var jsonClient = Substitute.For<IJsonClient>();
+            var client = new SearchClient(jsonClient);
+
+            const string Zap2ItId = "SH007501780000";
+
+            const string Route = "/search/series?zap2itId=SH007501780000";
+
+            var expectedData = new TvDbResponse<SeriesSearchResult[]>();
+
+            jsonClient.GetJsonAsync<TvDbResponse<SeriesSearchResult[]>>(Route, CancellationToken.None).Returns(expectedData);
+
+            var responseData = await client.SearchSeriesByZap2ItIdAsync(Zap2ItId, CancellationToken.None);
+
+            await jsonClient.Received().GetJsonAsync<TvDbResponse<SeriesSearchResult[]>>(Route, CancellationToken.None);
+
+            Assert.Equal(expectedData, responseData);
+        }
     }
 }
