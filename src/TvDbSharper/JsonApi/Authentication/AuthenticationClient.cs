b1b318a34bcc40137d9bdb8eb95645e5d8dcd31a
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-10-07 13:14:40 +0300

    added AuthenticationClient.AuthenticateAsync overload

--- src/TvDbSharper/JsonApi/Authentication/AuthenticationClient.cs
+++ src/TvDbSharper/JsonApi/Authentication/AuthenticationClient.cs
@@ -29,6 +29,41 @@
             this.UpdateAuthenticationHeader(response.Token);
         }
 
+        public async Task AuthenticateAsync(string apiKey, string username, string userKey, CancellationToken cancellationToken)
+        {
+            if (apiKey == null)
+            {
+                throw new ArgumentNullException(nameof(apiKey));
+            }
+
+            if (string.IsNullOrWhiteSpace(apiKey))
+            {
+                throw new ArgumentException("The ApiKey cannot be an empty string or white space.");
+            }
+
+            if (username == null)
+            {
+                throw new ArgumentNullException(nameof(username));
+            }
+
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                throw new ArgumentException("The Username cannot be an empty string or white space.");
+            }
+
+            if (userKey == null)
+            {
+                throw new ArgumentNullException(nameof(userKey));
+            }
+
+            if (string.IsNullOrWhiteSpace(userKey))
+            {
+                throw new ArgumentException("The UserKey cannot be an empty string or white space.");
+            }
+
+            await this.AuthenticateAsync(new AuthenticationRequest(apiKey, username, userKey), cancellationToken);
+        }
+
         public async Task RefreshTokenAsync(CancellationToken cancellationToken)
         {
             var response = await this.JsonClient.GetJsonAsync<AuthenticationResponse>("/refresh_token", cancellationToken);
--- test/TvDbSharper.Tests/AuthenticationClientTest.cs
+++ test/TvDbSharper.Tests/AuthenticationClientTest.cs
@@ -1,6 +1,7 @@
 ﻿namespace TvDbSharper.Tests
 {
     using System;
+    using System.Linq.Expressions;
     using System.Threading;
 
     using NSubstitute;
@@ -23,7 +24,7 @@
 
             const string Route = "/login";
 
-            var authenticationRequest = new AuthenticationRequest("ApiKey", "UserKey", "Username");
+            var authenticationRequest = new AuthenticationRequest("ApiKey", "Username", "UserKey");
 
             var response = new AuthenticationResponse("token_content");
 
@@ -69,6 +70,144 @@
         [Fact]
 
         // ReSharper disable once InconsistentNaming
+        public async void AuthenticateAsync_With_Inline_Login_Data_Makes_The_Right_Request()
+        {
+            var jsonClient = Substitute.For<IJsonClient>();
+            var client = new AuthenticationClient(jsonClient);
+
+            const string Route = "/login";
+
+            const string ApiKey = "ApiKey";
+            const string Username = "Username";
+            const string UserKey = "UserKey";
+
+            var response = new AuthenticationResponse("token_content");
+            var defaultResponse = new AuthenticationResponse("default_token_content");
+
+            Expression<Predicate<AuthenticationRequest>> requestCheck =
+                request => (request.ApiKey == ApiKey) && (request.Username == Username) && (request.UserKey == UserKey);
+
+            jsonClient.PostJsonAsync<AuthenticationResponse>(Route, Arg.Is(requestCheck), CancellationToken.None)
+                      .ReturnsForAnyArgs(defaultResponse);
+
+            jsonClient.PostJsonAsync<AuthenticationResponse>(Route, Arg.Is(requestCheck), CancellationToken.None).Returns(response);
+
+            await client.AuthenticateAsync(ApiKey, Username, UserKey, CancellationToken.None);
+
+            await jsonClient.Received().PostJsonAsync<AuthenticationResponse>(Route, Arg.Is(requestCheck), CancellationToken.None);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+        [InlineData("\t")]
+        [InlineData("\r")]
+        [InlineData("\n")]
+
+        // ReSharper disable once InconsistentNaming
+        public async void AuthenticateAsync_With_Inline_Login_Data_Throws_When_Passed_Empty_ApiKey(string value)
+        {
+            var client = CreateClient();
+
+            const string Username = "Username";
+            const string UserKey = "UserKey";
+
+            await
+                Assert.ThrowsAsync<ArgumentException>(
+                    async () => await client.AuthenticateAsync(value, Username, UserKey, CancellationToken.None));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+        [InlineData("\t")]
+        [InlineData("\r")]
+        [InlineData("\n")]
+
+        // ReSharper disable once InconsistentNaming
+        public async void AuthenticateAsync_With_Inline_Login_Data_Throws_When_Passed_Empty_UserKey(string value)
+        {
+            var client = CreateClient();
+
+            const string ApiKey = "ApiKey";
+            const string Username = "Username";
+
+            await
+                Assert.ThrowsAsync<ArgumentException>(
+                    async () => await client.AuthenticateAsync(ApiKey, Username, value, CancellationToken.None));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+        [InlineData("\t")]
+        [InlineData("\r")]
+        [InlineData("\n")]
+
+        // ReSharper disable once InconsistentNaming
+        public async void AuthenticateAsync_With_Inline_Login_Data_Throws_When_Passed_Empty_Username(string value)
+        {
+            var client = CreateClient();
+
+            const string ApiKey = "ApiKey";
+            const string UserKey = "UserKey";
+
+            await
+                Assert.ThrowsAsync<ArgumentException>(
+                    async () => await client.AuthenticateAsync(ApiKey, value, UserKey, CancellationToken.None));
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async void AuthenticateAsync_With_Inline_Login_Data_Throws_When_Passed_Null_ApiKey()
+        {
+            var client = CreateClient();
+
+            const string Username = "Username";
+            const string UserKey = "UserKey";
+
+            await
+                Assert.ThrowsAsync<ArgumentNullException>(
+                    async () => await client.AuthenticateAsync(null, Username, UserKey, CancellationToken.None));
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async void AuthenticateAsync_With_Inline_Login_Data_Throws_When_Passed_Null_UserKey()
+        {
+            var client = CreateClient();
+
+            const string ApiKey = "ApiKey";
+            const string Username = "Username";
+
+            await
+                Assert.ThrowsAsync<ArgumentNullException>(
+                    async () => await client.AuthenticateAsync(ApiKey, Username, null, CancellationToken.None));
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async void AuthenticateAsync_With_Inline_Login_Data_Throws_When_Passed_Null_Username()
+        {
+            var client = CreateClient();
+
+            const string ApiKey = "ApiKey";
+            const string UserKey = "UserKey";
+
+            await
+                Assert.ThrowsAsync<ArgumentNullException>(
+                    async () => await client.AuthenticateAsync(ApiKey, null, UserKey, CancellationToken.None));
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
         public async void RefreshTokenAsync_Makes_The_Right_Request()
         {
             var jsonClient = Substitute.For<IJsonClient>();
@@ -103,6 +242,11 @@
 
             Assert.Equal("Bearer", jsonClient.AuthorizationHeader.Scheme);
             Assert.Equal("token_content", jsonClient.AuthorizationHeader.Parameter);
+        }
+
+        private static AuthenticationClient CreateClient()
+        {
+            return new AuthenticationClient(Substitute.For<IJsonClient>());
         }
     }
 }
