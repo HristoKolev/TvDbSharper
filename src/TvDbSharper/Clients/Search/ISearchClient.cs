af2152725d51725b2cb0aa4d1fa5ed7cf2f88e6a
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-10-12 10:59:50 +0300

    added docs for the search client

--- src/TvDbSharper/Clients/Search/ISearchClient.cs
+++ src/TvDbSharper/Clients/Search/ISearchClient.cs
@@ -11,23 +11,79 @@
     /// </summary>
     public interface ISearchClient
     {
+        /// <summary>
+        /// <para>[GET /search/series]</para>
+        /// <para>Allows the user to search for a series based on the following parameters.</para>
+        /// </summary>
+        /// <param name="parameterValue">The parameter value</param>
+        /// <param name="parameterKey">An enum used for searching for series with <see cref="T:ISearchClient.SearchSeriesAsync"/>,
+        /// each value represents a property by which the search is performed</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesAsync(
-            string value,
-            SearchParameter parameter,
+            string parameterValue,
+            SearchParameter parameterKey,
             CancellationToken cancellationToken);
 
+        /// <summary>
+        /// <para>[GET /search/series]</para>
+        /// <para>Allows the user to search for a series based on the following parameters</para>
+        /// </summary>
+        /// <param name="parameterValue">The parameter value</param>
+        /// <param name="parameterKey">An enum used for searching for series with <see cref="T:ISearchClient.SearchSeriesAsync"/>,
+        /// each value represents a property by which the search is performed</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesAsync(string parameterValue, SearchParameter parameterKey);
 
+        /// <summary>
+        /// <para>[GET /search/series]</para>
+        /// <para>Allows the user to search for a series based on their imdb ID</para>
+        /// </summary>
+        /// <param name="imdbId">The imdb ID of the series</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesByImdbIdAsync(string imdbId, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// <para>[GET /search/series]</para>
+        /// <para>Allows the user to search for a series based on their imdb ID</para>
+        /// </summary>
+        /// <param name="imdbId">The imdb ID of the series</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesByImdbIdAsync(string imdbId);
 
+        /// <summary>
+        /// <para>[GET /search/series]</para>
+        /// <para>Allows the user to search for a series based on their name</para>
+        /// </summary>
+        /// <param name="name">The series name</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesByNameAsync(string name, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// <para>[GET /search/series]</para>
+        /// <para>Allows the user to search for a series based on their name</para>
+        /// </summary>
+        /// <param name="name">The series name</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesByNameAsync(string name);
 
+        /// <summary>
+        /// <para>[GET /search/series]</para>
+        /// <para>Allows the user to search for a series based on their Zap2It ID</para>
+        /// </summary>
+        /// <param name="zap2ItId">The Zap2It ID of the series</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesByZap2ItIdAsync(string zap2ItId, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// <para>[GET /search/series]</para>
+        /// <para>Allows the user to search for a series based on their Zap2It ID</para>
+        /// </summary>
+        /// <param name="zap2ItId">The Zap2It ID of the series</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<SeriesSearchResult[]>> SearchSeriesByZap2ItIdAsync(string zap2ItId);
     }
 }
--- src/TvDbSharper/Clients/Search/Json/SearchParameter.cs
+++ src/TvDbSharper/Clients/Search/Json/SearchParameter.cs
@@ -1,5 +1,9 @@
 ﻿namespace TvDbSharper.Clients.Search.Json
 {
+    /// <summary>
+    /// An enum used for searching for series with <see cref="T:ISearchClient.SearchSeriesAsync"/>,
+    /// each value represents a property by which the search is performed
+    /// </summary>
     public enum SearchParameter
     {
         Name,
