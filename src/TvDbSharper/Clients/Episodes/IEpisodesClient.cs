97e398243de17d4ddff1474762d4a36a4438ee03
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-10-12 09:34:43 +0300

    added docs to IEpisodesClient

--- src/TvDbSharper/Clients/Episodes/IEpisodesClient.cs
+++ src/TvDbSharper/Clients/Episodes/IEpisodesClient.cs
@@ -11,8 +11,19 @@
     /// </summary>
     public interface IEpisodesClient
     {
+        /// <summary>
+        /// Returns the full information for a given episode ID.
+        /// </summary>
+        /// <param name="episodeId">The episode ID</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<EpisodeRecord>> GetAsync(int episodeId, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Returns the full information for a given episode ID.
+        /// </summary>
+        /// <param name="episodeId">The episode ID</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />.The task object representing the asynchronous operation.</returns>
         Task<TvDbResponse<EpisodeRecord>> GetAsync(int episodeId);
     }
 }
