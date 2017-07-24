b661e38002f7dd088f624b531c510971f44f8cb1
HristoKolev <hristo.a.kolev@gmail.com>
2017-07-24 19:34:36 +0300

    search client

--- test/TvDbSharper.Tests/SearchClientTest.cs
+++ test/TvDbSharper.Tests/SearchClientTest.cs
@@ -40,6 +40,89 @@
         }
 
 
+        [Theory]
+        [InlineData("Doctor Who", "Doctor+Who")]
+        [InlineData("Sherlock", "Sherlock")]
+
+        // ReSharper disable once InconsistentNaming
+        public Task SearchSeriesByNameAsync_Makes_The_Right_Request(string name, string encodedName)
+        {
+            return CreateClient()
+                .WhenCallingAMethod((impl, token) => impl.SearchSeriesByNameAsync(name, token))
+                .ShouldRequest("GET", $"/search/series?name={encodedName}")
+                .RunAsync();
+        }
+
+        [Theory]
+        [InlineData("Doctor Who", "Doctor+Who")]
+        [InlineData("Sherlock", "Sherlock")]
+
+        // ReSharper disable once InconsistentNaming
+        public Task SearchSeriesByNameAsync_Without_CT_Makes_The_Right_Request(string name, string encodedName)
+        {
+            return CreateClient()
+                .WhenCallingAMethod((impl, token) => impl.SearchSeriesByNameAsync(name))
+                .ShouldRequest("GET", $"/search/series?name={encodedName}")
+                .WithNoCancellationToken()
+                .RunAsync();
+        }
+
+
+        [Theory]
+        [InlineData("tt0436992")]
+        [InlineData("tt0118480")]
+
+        // ReSharper disable once InconsistentNaming
+        public Task SearchSeriesByImdbIdAsync_Makes_The_Right_Request(string imdbId)
+        {
+            return CreateClient()
+                .WhenCallingAMethod((impl, token) => impl.SearchSeriesByImdbIdAsync(imdbId, token))
+                .ShouldRequest("GET", $"/search/series?imdbId={imdbId}")
+                .RunAsync();
+        }
+
+        [Theory]
+        [InlineData("tt0436992")]
+        [InlineData("tt0118480")]
+
+        // ReSharper disable once InconsistentNaming
+        public Task SearchSeriesByImdbIdAsync_Without_CT_Makes_The_Right_Request(string imdbId)
+        {
+            return CreateClient()
+                .WhenCallingAMethod((impl, token) => impl.SearchSeriesByImdbIdAsync(imdbId))
+                .ShouldRequest("GET", $"/search/series?imdbId={imdbId}")
+                .WithNoCancellationToken()
+                .RunAsync();
+        }
+
+
+        [Theory]
+        [InlineData("EP00225421")]
+        [InlineData("EP00750178")]
+
+        // ReSharper disable once InconsistentNaming
+        public Task SearchSeriesByZap2ItIdAsync_Makes_The_Right_Request(string zap2itId)
+        {
+            return CreateClient()
+                .WhenCallingAMethod((impl, token) => impl.SearchSeriesByZap2ItIdAsync(zap2itId, token))
+                .ShouldRequest("GET", $"/search/series?zap2itId={zap2itId}")
+                .RunAsync();
+        }
+
+        [Theory]
+        [InlineData("EP00225421")]
+        [InlineData("EP00750178")]
+
+        // ReSharper disable once InconsistentNaming
+        public Task SearchSeriesByZap2ItIdAsync_Without_CT_Makes_The_Right_Request(string zap2itId)
+        {
+            return CreateClient()
+                .WhenCallingAMethod((impl, token) => impl.SearchSeriesByZap2ItIdAsync(zap2itId))
+                .ShouldRequest("GET", $"/search/series?zap2itId={zap2itId}")
+                .WithNoCancellationToken()
+                .RunAsync();
+        }
+
         private static ApiTest<SearchClient> CreateClient()
         {
             return new ApiTest<SearchClient>()
